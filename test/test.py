c1d481769e1554b8140591b9e005bb8943d47634
MomokaIchi <143499383+MomokaIchi@users.noreply.github.com>
2025-12-23 07:16:07 +0900

    init

---
+++ test/test.py
@@ -0,0 +1,11 @@
+num = 2
+
+# print("ichi", num, "san")
+s = 'Alice'
+i = 25
+
+# print('{} is {} years old and {}'.format(s,i,i))
+
+# print('%s is %d years old' % (s,i))
+
+print(f'{s} is {i} years old')
