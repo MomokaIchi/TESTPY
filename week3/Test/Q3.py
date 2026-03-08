a8013c160adaa4d3bddf171e2703743f19b574eb
Ichikawa <momoka.ichikawa@honeywell.com>
2026-03-08 16:43:10 +0900

    8th SUNDAY

--- week3/Test/Q3.py
+++ week3/Test/Q3.py
@@ -10,3 +10,11 @@ class User:
 
 # if you change User.role = "admin", what happens to existing objects?
 # Why?
+
+# Class value can't be changed by a code that is outside of User
+# Instance value can be changed by a code that is outside of User
+
+# Even if I change role = "guest" to "admin", existing role is still "guest" 
+# because existing name is already created and there is no way to update. 
+# 
+# # I have to run again when change the admin
--- week3/Test/Q4.py
+++ week3/Test/Q4.py
@@ -1,9 +1,13 @@
 # What is inheritance?
 # if you have:
 
-class Admin(User):
-    pass
+# class Admin(User):
+#     pass
 
 # What does Admin automatically get from User?
 # Explain in therms of behavior reuse, not just "it inherits stuff"
 
+# Admin gets "self" from User class
+# To make easy to change only some user can change to Admin account
+
+# To categorize User to Admin and the other role
