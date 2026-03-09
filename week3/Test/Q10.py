7b8651c2288be3d59a14bc742ce80c812eafe128
Ichikawa <momoka.ichikawa@honeywell.com>
2026-03-10 07:22:23 +0900

    10th March

--- week3/Test/Q10.py
+++ week3/Test/Q10.py
@@ -6,3 +6,7 @@
 # Would inheritance make sence?
 # Would composition (having objects inseide objects) make more sense?
 # Explain your reasoning as a working design theory - not absolute truth
+
+# I'm going to make Normal user and Premium user inheritance class so they can change class
+# How ever, I'm going to make Admin user as a dependent class so Normal or Premium user can't chage
+# class to Admin
